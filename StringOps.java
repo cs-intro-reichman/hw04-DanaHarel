17c798056be51a3bd57464461cd2931860669da3
Dana Harel <dana.harel01@post.runi.ac.il>
2024-01-18 08:35:58 +0200

    fix for hw4

--- StringOps.java
+++ StringOps.java
@@ -68,102 +68,50 @@ public class StringOps {
     }
 
 
-    // public static String camelCase(String string) {
-    //     String finalStr = "";
-    //     //int i = 0;
-    //     // char c = string.charAt(i);
-    //     char p = ' ';
-    //     // if (Character.isWhitespace(c)) // c is white space 
-    //     // {
-    //     //     p = c;
-    //     // }
-    //     //  else
-    //     //  {
-    //     //     c = Character.toLowerCase(string.charAt(i)); // change c to lower case
-    //     //     finalStr = finalStr + c;
-    //     //     p = c;
-    //     //  }
+    public static String camelCase(String string) {
+       String finalStr = "";
+       int i = 0;
+       char c = string.charAt(i);
+       char p = ' ';
+       if (Character.isWhitespace(c)) // c is white space 
+         // {
+         //     p = c;
+         // }
+         //  {
+         //     c = Character.toLowerCase(string.charAt(i)); // change c to lower case
+         //     finalStr = finalStr + c;
+         //     p = c;
+         //  }
     
-    //     // loop for all String from i = 1
-    //     for (int i = 0; i < string.length(); i++) 
-    //     {
-    //         char c = string.charAt(i); // c is the character at place i in string
-    //         if (Character.isWhitespace(c)) // if c is white space
-    //         {
-    //             p = c;
-    //         }
-    //          else 
-    //          {
-    //             if (Character.isWhitespace(p)) // p (privious char) is white space 
-    //             {
-    //              c = Character.toUpperCase(string.charAt(i)); // change c to upper case
-    //              finalStr = finalStr + c;
-    //              p=c;
-    //             }
-    //              else
-    //              {
-    //                c = Character.toLowerCase(string.charAt(i)); // change c to lower case
-    //                finalStr = finalStr + c;
-    //                p = c;
-    //               }
+         // loop for all String from i = 1
+         for (int i = 0; i < string.length(); i++) 
+         {
+             char c = string.charAt(i); // c is the character at place i in string
+             if (Character.isWhitespace(c)) // if c is white space
+             {
+                 p = c;
+             }
+              else 
+              {
+                 if (Character.isWhitespace(p)) // p (privious char) is white space 
+                 {
+                  c = Character.toUpperCase(string.charAt(i)); // change c to upper case
+                  p=c;
+                 }
+                  else
+                  {
+                    c = Character.toLowerCase(string.charAt(i)); // change c to lower case
+                    p = c;
+                  }
 
-    //          }
+               }
             
-    //           } 
+           } 
 
-    //           return finalStr;
-    //     }
-
-    public static String camelCase (String str) 
-    {
-       
-        String res = "";
-        String temp = "";
-        int start = 0;
-        int stop = 0;
-        int c = 0; 
-        for(int i = 0; i<str.length(); i++)
-        {
-            if(str.charAt(i)== ' ')
-            {
-                c++;
-            }
-        }
-
-        //c = amountOfSpaces(str);
-        if(c == 0)// if there is no space, return the word in small leters
-        {
-            res = lowerCase(str);
-        }
-        else// there are some spaces
-        {
-           for(int i = 0; i<=c; i++)
-           {
-                if(start == 0)// makes the first word in small leters
-                {
-                    stop = str.indexOf(' ');
-                    res+= lowerCase(str.substring(start,stop));
-                    
-                }
-                else
-                {
-                    stop = str.indexOf(' ',start);
-                    if(stop == -1)
-                    {
-                        res+= makesItPerfect(str.substring(start));
-                    }
-                    else
-                    {
-                        res+= makesItPerfect(str.substring(start, stop));
-                        start = stop+1;
-                    }
-                }
-                start = stop+1;
-           }
-        }
-        return res ;
+           return finalStr;
     }
 
+    
     
 
     public static int[] allIndexOf (String string, char chr) {
@@ -191,79 +139,5 @@ public class StringOps {
         
         return arr;
     }
-
-
-    public static boolean isThereSpace(String str) 
-    {
-        boolean flag = false;
-        for(int i = 0; i<str.length(); i++)
-        {
-            if(str.charAt(i)!= ' ')
-            {
-                continue;
-            }
-            else
-            {
-                flag = true;
-                break;
-            }
-        }
-      
-        return flag;
-    }
-
-    public static String makesItPerfect(String str) 
-    {
-        String res = "";
-        char ch = str.charAt(0);
-        int ascii = ch ;
-        if (ascii>=97 && ascii<=122)// if small leter make it capital
-            {
-                int asciiC = ascii - 32; 
-                res = res + ((char)asciiC);
-            }
-        else
-        {
-            res += str.charAt(0);
-        }
-        for (int i = 1; i< str.length(); i++)
-        {
-            ch = str.charAt(i);
-            ascii = ch ;
-            if (ascii>=64 && ascii<=90)// if capital leter make it small
-            {
-                int asciiLow = ascii + 32; 
-                res = res + ((char)asciiLow);
-            }
-            else
-            {
-                res += str.charAt(i);
-            }
-        }
-      
-        return res;
-    }
-
-    public static String lowerCase(String s) 
-    {
-        
-        String res = "";
-        for (int i = 0; i< s.length(); i++)
-        {
-            char ch = s.charAt(i);
-            int ascii = ch ;
-            if (ascii>=65 && ascii<=94)
-            {
-                int asciiLow = ascii + 32; 
-                res = res + ((char)asciiLow);
-            }
-            else
-            {
-                res = res + ch;
-            }
-        }
-        return res;
-    }
-
 
 }
